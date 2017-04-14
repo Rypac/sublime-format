0a133bd164cfd1f347a1264e2d9c07ad5f11215f
Ryan Davis <me@rdavis.xyz>
2017-04-14 13:57:19 +1000

    Add shell option to command

--- src/command.py
+++ src/command.py
@@ -5,12 +5,13 @@ import os
 class Command():
     def __init__(self, command):
         self.__command = command
-        self.__input = input
         self.__startup_info = None
+        self.__shell = False
         if os.name == 'nt':
             self.__startup_info = subprocess.STARTUPINFO()
             self.__startup_info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
             self.__startup_info.wShowWindow = subprocess.SW_HIDE
+            self.__shell = True
 
     def run(self, input):
         return subprocess.Popen(
@@ -19,4 +20,5 @@ class Command():
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             startupinfo=self.__startup_info,
+            shell=self.__shell,
             universal_newlines=True).communicate(input=input)
