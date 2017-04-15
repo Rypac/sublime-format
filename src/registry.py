0e19f960b2234fcd9711f123526f8de507ed2d99
Ryan Davis <me@rdavis.xyz>
2017-04-15 10:21:27 +1000

    Speed up lookup of formatter by source type

--- src/registry.py
+++ src/registry.py
@@ -4,12 +4,16 @@ from .formatter import *
 class FormatterRegistry():
     def __init__(self):
         self.__formatters = []
+        self.__formatter_source_map = {}
 
     def populate(self):
         self.__formatters = [
             ClangFormat(), ElmFormat(), GoFormat(), JavaScriptFormat(),
             JsonFormat(), PythonFormat(), RustFormat(), TerraformFormat()
         ]
+        self.__formatter_source_map = dict((source, formatter)
+                                           for formatter in self.__formatters
+                                           for source in formatter.sources)
 
     @property
     def all(self):
@@ -19,12 +23,9 @@ class FormatterRegistry():
     def enabled(self):
         return [x for x in self.all if x.format_on_save]
 
-    def find(self, predicate, default=None):
-        return next((x for x in self.all if predicate(x)), default)
-
     def by_view(self, view):
         source = view.scope_name(0).split(' ')[0].split('.')[1]
-        return self.find(lambda x: source in x.sources)
+        return self.__formatter_source_map.get(source)
 
     def by_name(self, name):
-        return self.find(lambda x: x.name == name)
+        return next((x for x in self.all if x.name == name))
