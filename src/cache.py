c86df904ee9e0cbd68ee5791f14e2851a8d52f36
Ryan Davis <me@rdavis.xyz>
2017-04-15 20:01:56 +1000

    Remove leftover debug logs

--- src/cache.py
+++ src/cache.py
@@ -12,10 +12,7 @@ def cache(fn):
         value = self.__cache.get(key)
         if value is None:
             value = fn(self, *args, **kwargs)
-            print('Caching', key, 'with', value)
             self.__cache[key] = value
-        else:
-            print('Using', key, 'as', value)
         return value
 
     return wrapper
@@ -26,11 +23,8 @@ def invalidate(fn):
 
     @wraps(fn)
     def wrapper(self, *args, **kwargs):
-        if not hasattr(self, '__cache'):
-            self.__cache = {}
-
-        print('Invalidating', key)
-        self.__cache.pop(key, None)
+        if hasattr(self, '__cache'):
+            self.__cache.pop(key, None)
         fn(self, *args, **kwargs)
 
     return wrapper
