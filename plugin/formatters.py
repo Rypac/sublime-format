1f0f74e91ef96ed98ca184ecde7b67c345e8d5cd
Ryan Davis <me@rdavis.xyz>
2017-04-20 10:04:43 +1000

    Use view specific indentation size when formatting JSON

--- plugin/formatters.py
+++ plugin/formatters.py
@@ -2,9 +2,12 @@ import json
 from collections import OrderedDict
 
 
-def format_json(input):
+def format_json(input, settings=None):
+    indent = 4
+    if settings:
+        indent = settings.get('tab_size', indent)
     try:
         data = json.loads(input, object_pairs_hook=OrderedDict)
-        return json.dumps(data, indent=4), None
+        return json.dumps(data, indent=indent), None
     except ValueError:
         return None, 'Invalid JSON'
