2479e37be5ef2c99c716ceebb1cc9af41a3e95ed
Ryan Davis <ryan@rypac.org>
2023-03-28 17:34:24 +1100

    Update plugin imports

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -2,4 +2,5 @@ from . import configuration, formatter, registry, settings, shell, view
 
 from .configuration import Configuration
 from .formatter import Formatter
-from .registry import FormatterRegistry
+from .registry import FormatterRegistry, WindowFormatterRegistry
+from .settings import FormatterSettings
